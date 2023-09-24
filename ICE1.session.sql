277c40cf9cfe7beaad1fb78fc79de538b7dc209f
NancyDungrani <113873693+NancyDungrani@users.noreply.github.com>
2023-09-24 19:31:34 -0400

    Add files via upload

---
+++ ICE1.session.sql
@@ -0,0 +1,12 @@
+INSERT INTO movie ( Title , year , rating , description)
+values ('the material',
+'1999',
+ 8.9,
+'sci-fi')
+
+
+INSERT INTO movie ( Title , year , rating , description)
+values ('The Uncanny COunter',
+'2023',
+ 9.9,
+'fiction')
